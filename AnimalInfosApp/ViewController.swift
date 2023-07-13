5060844811ea828685b8053a424239ba841ab8ac
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-07-14 00:50:28 +0300

    Initial Commit

---
+++ AnimalInfosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalInfosApp
+//
+//  Created by Aleyna Aktaş on 14.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
